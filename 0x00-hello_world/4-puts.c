8befc7ca4e8b433ff9c8d654bcdc3d6de1fb3789
Francis_Kwofie <francisskwofie01@gmail.com>
2022-03-10 16:09:01 -0800

    Task 4 down

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - prints the phrase "Programming is like building a multinational puzzle
- * Return: 0 if exited properly, non-zero otherwis
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
