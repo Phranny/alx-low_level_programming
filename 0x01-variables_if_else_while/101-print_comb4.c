bbaff77c841be6ef84f0da49a5f9731556cb792a
Francis_Kwofie <francisskwofie01@gmail.com>
2022-03-14 07:55:43 -0700

    Task_11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -2,7 +2,7 @@
 
 /**
  * main - prints all possible combination of three digits
- * Return: Always 9 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -10,11 +10,11 @@ int main(void)
 
 	for (n = 48; n < 58; n++)
 	{
-		for (m = 49: m < 58; m++)
+		for (m = 49; m < 58; m++)
 		{
 			for (l = 50; l < 58; l++)
 			{
-				if (l > m && m >n)
+				if (l > m && m > n)
 				{
 					putchar(n);
 					putchar(m);
