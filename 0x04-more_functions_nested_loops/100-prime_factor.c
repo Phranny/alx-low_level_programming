d3b76c3bb79f0658d74e736d55e66135b289a06a
Francis_Kwofie <francisskwofie01@gmail.com>
2022-03-17 21:16:30 -0700

    Advanced task corrected

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,10 +1,30 @@
 #include <stdio.h>
+
 /**
- * main - prints prime of 612852475143
- *
- * Return: Always 0 (Success)
- */
+* main - prints the largest prime factor of the number 612852475143
+*
+* Return: Always 0
+*/
+
 int main(void)
 {
-	return (0)
+	long int prime = 612852475143;
+	long int quotient = prime;
+	long int divisor = 2;
+
+	while (quotient != divisor)
+	{
+		if (quotient % divisor == 0)
+		{
+			quotient = quotient / divisor;
+		}
+		else
+		{
+			divisor++;
+		}
+	}
+
+	printf("%li\n", quotient);
+
+	return (0);
 }
--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,10 +1,21 @@
 #include "main.h"
+
 /**
- * print_number - a fuction that prints an integer
- * @n: input
- * Return: inputted integer
- */
+* print_number - Prints an integer.
+* @n: The integer to be printed.
+*/
 void print_number(int n)
 {
-	return (0);
+	unsigned int num = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -num;
+	}
+
+	if ((num / 10) > 0)
+		print_number(num / 10);
+
+	_putchar((num % 10) + '0');
 }
