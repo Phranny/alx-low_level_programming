bf751643c23f4357c4c9dc0f7b1f716d367c178c
Francis_Kwofie <francisskwofie01@gmail.com>
2022-04-02 15:37:21 -0700

    Task_4,5

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -6,7 +6,7 @@
 *        make change for an amount of money.
 * @argc: The number of arguments supplied to the program.
 * @argv: An array of pointers to the arguments.
-* 
+*
 * Return: If the number of arguments is not exactly one - 1.
 *         Otherwise - 0.
 */
