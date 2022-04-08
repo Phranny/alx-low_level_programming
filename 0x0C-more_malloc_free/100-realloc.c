82deba39c43fd8cbc4afaaacb10179441fdbd559
Francis_Kwofie <francisskwofie01@gmail.com>
2022-04-07 21:40:37 -0700

    Bugs resolved

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -23,8 +23,9 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	{
 		nptr = malloc(new_size);
 
-	if (nptr == NULL)
-		return (NULL);
+		if (nptr == NULL)
+			return (NULL);
+	
 		return (nptr);
 	}
 	else
